11c712aa680371d4fb4a7c31811d08a1f0f8cd5b
EmprendeTech <tu@emprendetech.com>
2026-06-26 06:29:52 -0600

    v7.71: Arreglar redirección botón Conectar Mercado Pago - usar <button> con onClick={() => window.location.href = '/api/oauth/mercadopago/start'} para redirigir correctamente al flujo OAuth. Commit v7.71 y push.

--- lib/constants/version.ts
+++ lib/constants/version.ts
@@ -3,5 +3,5 @@
  * Sirve para confirmar QUÉ build estás viendo en producción.
  * Súbelo cada vez que despliegues cambios grandes.
  */
-export const APP_VERSION = 'v7.70'
-export const APP_BUILD = '2026-06-26' // v7.70: Conexión simple sin tokens manuales - Quitar input "Guardar token", botón grande "Conectar mi cuenta de Mercado Pago" vía OAuth en StorePaymentSection (visible en General para todos los planes). Commit v7.70 push.
+export const APP_VERSION = 'v7.71'
+export const APP_BUILD = '2026-06-26' // v7.71: Arreglar redirección botón Conectar MP - usar <button> con onClick window.location.href = '/api/oauth/mercadopago/start' para redirigir correctamente al OAuth. Commit v7.71 push.
